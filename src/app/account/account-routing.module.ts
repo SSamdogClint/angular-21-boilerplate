06732a86e750138b4a4d9cc728a7241a87077cea
SSamdogClint <capondag.clint117@gmail.com>
2026-05-01 18:23:18 +0800

    feat(account): implement login, register, forgot password, reset password, verify email, and layout components

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+import { VerifyEmailComponent } from './verify-email.component';
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { ResetPasswordComponent } from './reset-password.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent},
+      { path: 'verify-email', component: VerifyEmailComponent},
+      { path: 'forgot-password', component: ForgotPasswordComponent},
+      { path: 'reset-password', component: ResetPasswordComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
