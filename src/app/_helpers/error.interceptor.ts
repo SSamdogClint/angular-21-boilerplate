bb538bf9de1d5954df694e26157cc7b24ee7a256
SSamdogClint <capondag.clint117@gmail.com>
2026-05-03 15:11:32 +0800

    chore: remove unused retry import from error interceptor

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 
 import { AccountService } from '@app/_services';
 
