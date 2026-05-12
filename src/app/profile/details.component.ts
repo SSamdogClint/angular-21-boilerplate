fa198b2afde7439a8fa4e78866cda02b21d1f963
SSamdogClint <capondag.clint117@gmail.com>
2026-05-13 01:01:00 +0800

    FixFix profile details template filename casing

--- src/app/profile/details.component.ts
+++ src/app/profile/details.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { AccountService } from '@app/_services';
 
-@Component({ templateUrl: 'details.Component.html', standalone: false })
+@Component({ templateUrl: 'details.component.html', standalone: false })
 export class DetailsComponent {
     constructor(private accountService: AccountService) { }
 
